1a51d9ede7f9e12d402002674baf6a9152cc4976
Elisa Martinez <97morningstar@gmail.com>
2018-01-22 10:21:06 -0600

    adding the seeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use App\User;
+use App\Role;
 
 class DatabaseSeeder extends Seeder
 {
@@ -11,6 +13,25 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UsersTableSeeder::class);
+        User::truncate();
+        Role::truncate();
+        DB::table('assigned_roles')->truncate();
+
+
+        $user = User::create([
+        	'name' => 'Elisa',
+        	'email' => 'admin@admin.com',
+        	'password' => '123123'
+        ]);
+
+        $role = Role::create([
+        	'name' => 'admin',
+        	'display_name' => 'Administrador',
+        	'description' => 'Adminitrador del sitio'
+         ]);
+
+        $user->roles()->save($role);
+
+
     }
 }
